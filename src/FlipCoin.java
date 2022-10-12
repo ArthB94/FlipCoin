f11c017fc9aeb4264ec6aaacd2ba6cf905927615
ArthB94 <arthur.billebaut@efrei.net>
2022-10-12 04:43:04 +0200

    init

--- src/FlipCoin.java
+++ src/FlipCoin.java
@@ -1,5 +1,5 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class FlipCoin extends Application {
+    public static void main(String[] args) {
+
     }
 }
